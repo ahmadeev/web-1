14f5ef8a08b1c9b8ec857ded4cb2022eda1a6b8a
danis <ahrnadeev@gmail.com>
2023-10-19 05:09:05 +0300

    7th

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="icon" href="assets/img/icon.png" type="image/png">
     <script type="text/javascript" src="js/jquery.min.js"></script>
+    <!-- <script src="js/validate.js"></script> -->
 </head>
 
 <body bgcolor="24190f">
@@ -18,64 +19,61 @@
         <div id="contentLeft">
             
             <form method="post" id="form">
-                <div id="inputValues">
+                <div class="formLine"><label>x value:</label></div>
+                <div class="formLine">
                     
-                    <table width="180">
-                        <tr><label>x value:</label></tr>
-                        <tr>
-                            <td><input type="radio" onchange="pupu()" id="rB_3" name="xType" value="-3" />
-                            <label for="rB_3">-3</label></td>
-                            <td><input type="radio" onchange="pupu()" id="rB_2" name="xType" value="-2" />
-                            <label for="rB_2">-2</label></td>
-                            <td><input type="radio" onchange="pupu()" id="rB_1" name="xType" value="-1" />
-                            <label for="rB_1">-1</label></td>
-                        </tr>
-                        <tr>
-                            <td><input type="radio" onchange="pupu()" id="rB0" name="xType" value="0" />
-                            <label for="rB0">0</label></td>
-                            <td><input type="radio" onchange="pupu()" id="rB1" name="xType" value="1" />
-                            <label for="rB1">1</label></td>
-                            <td><input type="radio" onchange="pupu()" id="rB2" name="xType" value="2" />
-                            <label for="rB2">2</label></td>
-                        </tr>
-                        <tr>
-                            <td><input type="radio" onchange="pupu()" id="rB3" name="xType" value="3" />
-                            <label for="rB3">3</label></td>
-                            <td><input type="radio" onchange="pupu()" id="rB4" name="xType" value="4" />
-                            <label for="rB4">4</label></td>
-                            <td><input type="radio" onchange="pupu()" id="rB5" name="xType" value="5" />
-                            <label for="rB5">5</label></td>
-                        </tr>
-                    </table>
+                    <label><input type="radio" onchange="pupu()" class="radio1" name="xType" value="-3" />-3</label>
+                    <label><input type="radio" onchange="pupu()" class="radio2" name="xType" value=" 0" />0</label>
+                    <label><input type="radio" onchange="pupu()" class="radio3" name="xType" value="3" />3</label>
+                    
+                          
+                </div>
+                <div class="formLine">
+                    <label><input type="radio" onchange="pupu()" class="radio1" name="xType" value="-2" />-2</label>
+                    <label><input type="radio" onchange="pupu()" class="radio2" name="xType" value=" 1" />1</label>
+                    <label><input type="radio" onchange="pupu()" class="radio3" name="xType" value="4" />4</label>
+                    
+                          
+                </div>
+                <div class="formLine">
+                    <label><input type="radio" onchange="pupu()" class="radio1" name="xType" value="-1" />-1</label>  
+                    <label><input type="radio" onchange="pupu()" class="radio2" name="xType" value=" 2" />2</label>  
+                    <label><input type="radio" onchange="pupu()" class="radio3" name="xType" value="5" />5</label> 
+                    
+                    
+                                  
+                </div>
+                <div class="formLine"><label>y value:</label></div>
+                <div class="formLine">
 
-                    <table width="180">
-                        <tr><td><label>y value:</label></td></tr>
-                        <tr><td><input type="textbox" onchange="pupu()" id="textingtext" name="yType" placeholder="-5 to 3" autocomplete="off"></td></tr>
-                    </table>
-
-                    <table width="190">
-                        <tr><label>R value:</label></tr>
-                        <td><input type="checkbox" onchange="pupu()" id="checkbox1" name="RType" value="1">
-                        <label for="checkbox1">1</label></td>
-                        <td><input type="checkbox" onchange="pupu()" id="checkbox2" name="RType" value="2">
-                        <label for="checkbox2">2</label></td>
-                        <td><input type="checkbox" onchange="pupu()" id="checkbox3" name="RType" value="3">
-                        <label for="checkbox3">3</label></td>
-                        <td><input type="checkbox" onchange="pupu()" id="checkbox4" name="RType" value="4">
-                        <label for="checkbox4">4</label></td>
-                        <td><input type="checkbox" onchange="pupu()" id="checkbox5" name="RType" value="5">
-                        <label for="checkbox5">5</label></td>
-                    </table>
-
+                    <input type="textbox" id="textingtext" name="yType" placeholder="-5 to 3" autocomplete="off">
+                </div>
+                <div class="formLine"><label>R value:</label></div>
+                <div class="formLine">
+                    
+                    <input type="checkbox" onchange="pupu()" name="RType" value="1">
+                    <label for="checkbox1">1</label>
+                    <input type="checkbox" onchange="pupu()" name="RType" value="2">
+                    <label for="checkbox2">2</label>
+                    <input type="checkbox" onchange="pupu()" name="RType" value="3">
+                    <label for="checkbox3">3</label>
+                    <input type="checkbox" onchange="pupu()" name="RType" value="4">
+                    <label for="checkbox4">4</label>
+                    <input type="checkbox" onchange="pupu()" name="RType" value="5">
+                    <label for="checkbox5">5</label>
+                </div>
+                <div class="formLine">
                     <button type="submit" disabled="" id="submitButton" name="submitButton">Submit</button>
                     <button type="reset" id="resetButton" name="resetButton">Reset</button>
                 </div>
-            </form>
+            </form>  
             
         </div>
         <div id="contentRight">
             <img src="assets/img/img.png" id="img">
+              
         </div>
+            
     </div>
     <div id="contentCenter">
         <table align="center" id="resultTable">
@@ -94,27 +92,27 @@
 
     
 </div>
-<div id="contentBottom"></div>
+<!-- <div id="contentBottom"></div> -->
 
 <script src="js/POST + rowInsert.js"></script>
 <script src="js/checkboxes.js"></script>
-
 <script>
     function pupu() {
-        var submitButton = form.querySelector('#submitButton');
-        var xInput = form.querySelector('input[name="xType"]:checked').value;
-        var yInput = form.querySelector('[name="yType"]').value;
-        var RInput = form.querySelector('input[name="RType"]:checked').value;
+    var submitButton = form.querySelector('#submitButton');
+    var xInput = form.querySelector('input[name="xType"]:checked').value;
+    var yInput = form.querySelector('[name="yType"]').value;
+    var RInput = form.querySelector('input[name="RType"]:checked').value;
 
-        if (parseInt(yInput) <= 3 && parseInt(yInput) >= -5 && parseInt(yInput) != null && parseInt(xInput) != null && parseInt(RInput) != null) {
-            submitButton.removeAttribute("disabled");
-            
-        } else {
-            submitButton.setAttribute("disabled", "");
-        }
+    if (parseInt(yInput) <= 3 && parseInt(yInput) >= -5 && parseInt(yInput) != null && parseInt(xInput) != null && parseInt(RInput) != null) {
+        submitButton.removeAttribute("disabled");
         
+    } else {
+        submitButton.setAttribute("disabled", "");
     }
+    
+}
 </script>
+
 
 </body>
 </html>
